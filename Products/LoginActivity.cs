bdccb5441e2ded116aaa0bfbef8145ffd3738fae
Martin Pernica <martin.developer@hotmail.com>
2015-11-26 18:26:43 +0100

    Initial commit

---
+++ Products/LoginActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Products
+{
+	[Activity (Label = "Products", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class LoginActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+		}
+	}
+}
+
+
